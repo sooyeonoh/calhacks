70b9bb628f567798e88300dc2c3e4923e34766a7
Sooyeon <sooyeonoh48@gmail.com>
2021-10-22 21:54:32 -0700

    Create navigation bar

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav';
+
+function Dashboard(props) {
+    return (
+        <div>
+            <Nav/>
+            
+        </div>
+    );
+}
+
+export default Dashboard;
---
+++ client/src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+function NavBar(props) {
+    return (
+        <div className="ps-5 pe-5 d-flex align-items-center justify-content-between" style={{height: "80px", outline:"1px solid lightGray"}}>
+            <div className="d-flex flex-row align-items-center">
+                <img src="/bear.png" className="me-2" style={{width: "28px"}} alt="bear" />
+                <h3>CalDates</h3>
+            </div>
+            <div className="d-flex flex-row align-items-center">
+                <NavLink className="ps-4 navlink" to="/">Home</NavLink>
+                <NavLink className="ps-4 navlink" to="/">Matches</NavLink>
+                <NavLink className="ps-4 navlink" to="/">Profile</NavLink>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
