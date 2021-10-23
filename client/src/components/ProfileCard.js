bc2ca48fd917614b73c6448166758eee9ace0ae1
Sooyeon <sooyeonoh48@gmail.com>
2021-10-23 00:50:10 -0700

    Update profile cards

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -4,13 +4,35 @@ import InfoTag from './InfoTag';
 
 function ProfileCard(props) {
     return (
-        <Card style={{ width: '20rem', height: '500px', margin: "10px"}}>
+        <Card style={{ width: '20rem', height: '500px', margin: "10px", padding: '10px 15px'}}>
             <Card.Body>
-                <h1 style={{fontSize: '70px'}}>{props.data.emoji}</h1>
-                <Card.Title style={{fontSize: '28px'}}>{props.name}</Card.Title>
-                <Card.Text className="fst-italic">{props.data.bio}</Card.Text>
-                <InfoTag type="primary" info={props.data.year}/>
-                <InfoTag type="secondary" info={props.data.major}/>
+                <div className="mb-5">
+                    <h1 style={{fontSize: '70px'}}>{props.data.emoji}</h1>
+                    <Card.Title style={{fontSize: '28px'}}>{props.name}</Card.Title>
+                    <Card.Text className="fw-bold">{props.data.major}, {props.data.year}</Card.Text>
+                    <Card.Text className="fst-italic" style={{fontSize: '20px'}}>"{props.data.bio}"</Card.Text>
+                </div>
+
+                <div className="d-flex flex-column align-items-start">
+                    <div className="d-flex flex-column align-items-start mb-4">
+                        <Card.Title style={{fontSize: '18px'}}>Personality</Card.Title>
+                        <div>
+                            <InfoTag type="primary" info={props.interests.mbti}/>
+                            <InfoTag type="dark" info={props.interests.astrology}/>
+                            <InfoTag type="warning" info={props.interests.hogwarts}/>
+                        </div>
+                    </div>
+
+                    <div className="d-flex flex-column align-items-start mb-4">
+                        <Card.Title style={{fontSize: '18px'}}>Interests</Card.Title>
+                        <div>
+                            <InfoTag type="secondary" info={props.interests.music}/>
+                            <InfoTag type="secondary" info={props.interests.berkeley_restaurant}/>
+                        </div>
+                    </div>
+
+                </div>
+
             </Card.Body>
         </Card>
     );
