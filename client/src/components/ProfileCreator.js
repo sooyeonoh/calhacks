45df9db30cc3506847a421d611b9aacd8439deb8
Sage <sagepierone24@gmail.com>
2021-10-23 01:56:10 -0700

    Added emoji picker, name input, major input, and year input

--- client/src/components/ProfileCreator.js
+++ client/src/components/ProfileCreator.js
@@ -1,30 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './NavBar';
-import {
-    Button,
-    Checkbox,
-    Form,
-    Input,
-    Radio,
-    Select,
-    TextArea,
-} from 'semantic-ui-react'
+
+import Picker from 'emoji-picker-react';
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+const AvatarPicker = () => {
+    const [chosenEmoji, setChosenEmoji] = useState(null);
+
+    const onEmojiClick = (event, emojiObject) => {
+        setChosenEmoji(emojiObject);
+    };
+
+    return (
+        <div>
+            {chosenEmoji ? (
+                <span>You chose: {chosenEmoji.emoji}</span>
+            ) : (
+                <span>No emoji Chosen</span>
+            )}
+            <Picker onEmojiClick={onEmojiClick} />
+        </div>
+    );
+};
 
 class ProfileCreator extends React.Component {
-
-   
     render() {
         return (
             <div>
                 <NavBar/>
-                <Form>
-                    <label>
-                        <input
-                            type="text"
-                            placeholder={"Name"}
-                        />
-                    </label>
-                </Form>
+                <div class="profile-info">
+                    <AvatarPicker/>
+                    <div class="input-group first-name">
+                        <input type="text" class="form-control" placeholder="First Name" aria-label="First Name"/>
+                    </div>
+                    <div class="input-group last-name">
+                        <input type="text" class="form-control" placeholder="Last Name" aria-label="Last Name" />
+                    </div>
+                    <div class="input-group major">
+                        <input type="text" class="form-control" placeholder="Major" aria-label="Major" />
+                    </div>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            Select Year
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="#">Freshman</a></li>
+                            <li><a class="dropdown-item" href="#">Sophomore</a></li>
+                            <li><a class="dropdown-item" href="#">Junior</a></li>
+                            <li><a class="dropdown-item" href="#">Senior</a></li>
+                            <li><a class="dropdown-item" href="#">Graduate</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         );
     }
